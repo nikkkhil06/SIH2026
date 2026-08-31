e26e198c10094127d8f5fa548531da53eb0690eb
nikkkhil06 <nikhilvishwakarma0351@gmail.com>
2026-08-31 19:09:33 +0530

    Revert "ui completed"
    This reverts commit 30b1eb7fbd83f275641687599b4e95f5dd42a80d.

--- src/components/StatCard.jsx
+++
@@ -1,7 +0,0 @@
-export default function StatCard({title,value,description}){
- return <div className="card stat">
-    <span>{title}</span>
-    <strong>{value}</strong>
-    <small>{description}</small>
-  </div>;
-}
